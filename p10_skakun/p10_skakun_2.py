0d26c402f391796075bc6a692a4e64e5d9811b7d
Anton Skakun <ponanton2713@gmail.com>
2021-11-09 16:33:08 +0200

    p10

--- p10_skakun/p10_skakun_2.py
+++ p10_skakun/p10_skakun_2.py
@@ -22,39 +22,27 @@ def leap_year_detector(years):
         else:
             years_that_remain1.append(i)
     no_leap_years = list(filter(lambda year: year % 100 == 0, years_that_remain1))
-    years_that_remain3 = []
+    years_that_remain2 = []
     for i in years_that_remain1:
         if i in no_leap_years:
             continue
         else:
-            years_that_remain3.append(i)
-    leap_years_4 = list(filter(lambda year: year % 4 == 0, years_that_remain3))
-    years_that_remain4 = []
-    for i in years_that_remain3:
-        if i in leap_years_4:
-            continue
-        else:
-            years_that_remain4.append(i)
+            years_that_remain2.append(i)
+    leap_years_4 = list(filter(lambda year: year % 4 == 0, years_that_remain2))
     leap_years.extend(leap_years_4)
-    no_leap_years.extend(years_that_remain4)
-    return(leap_years, no_leap_years)
-
+    return(leap_years)
 def number_of_days_in_a_month(years, func, year, month):
-    tuple_of_years = func(years)
+    leap_years = func(years)
     months_days_31 = [1, 3, 5, 7, 8, 10, 12]
     months_days_30 = [4, 6, 9, 11]
     months_days_28_or_29 = [2]
-    if year in tuple_of_years[1] and month in months_days_31:
+    if month in months_days_31:
         print("Month number", month, "in", year, "has 31 days")
-    elif year in tuple_of_years[1] and month in months_days_30:
+    elif month in months_days_30:
         print("Month number", month, "in", year, "has 30 days")
-    elif year in tuple_of_years[1] and month in months_days_28_or_29:
+    elif year in leap_years and month in months_days_28_or_29:
         print("Month number", month, "in", year, "has 29 days")
-    elif year in tuple_of_years[2] and month in months_days_30:
-        print("Month number", month, "in", year, "has 30 days")
-    elif year in tuple_of_years[2] and month in months_days_31:
-        print("Month number", month, "in", year, "has 31 days")
-    elif year in tuple_of_years[2] and month in months_days_30:
+    elif month in months_days_28_or_29:
         print("Month number", month, "in", year, "has 28 days")
 
 number_of_days_in_a_month(years, leap_year_detector, year, month )
